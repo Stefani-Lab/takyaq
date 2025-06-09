5da84c50878d6cbc456d18d12337e3a65b8a108b
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2025-06-09 15:12:35 -0300

    WIP: clean debris

--- takyaq/wrappers/pylablib.py
+++ takyaq/wrappers/pylablib.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  9 11:08:00 2025
+Copyright (C) 2025 Andrés Zelcer and others
 
-@author: azelcer
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
-# from pylablib.devices import Thorlabs, Andor  # import the device libraries
-# import numpy as np  # import numpy for saving
-
-# # connect to the devices
-# with Thorlabs.KinesisMotor("27000000") as stage, Andor.AndorSDK2Camera() as cam:
-#    # change some camera parameters
-#    cam.set_exposure(50E-3)
-#    cam.set_roi(0, 128, 0, 128, hbin=2, vbin=2)
-#    # start the stepping loop
-#    images = []
-#    for _ in range(10):
-#       stage.move_by(10000)  # initiate a move
-#       stage.wait_move()  # wait until it's done
-#       img = cam.snap()  # grab a single frame
-#       images.append(img)
-
-# from pylablib.devices import uc480, NI
-
-# # Set the DLL path
-# dll_path = r"C:\Program Files\Thorlabs\Scientific Imaging\ThorCam"
-# os.environ["PATH"] += os.pathsep + dll_path
 
 from __future__ import annotations
 import logging as _lgn
-from ..base_classes import BaseCamera as _bc, BasePiezo  as _bp
+from ..base_classes import BaseCamera as _bc, BasePiezo as _bp
 from pylablib.devices.interface.camera import ICamera as _ICamera
 from pylablib.devices.Thorlabs.kinesis import BasicKinesisDevice as _Kinesis
 
@@ -93,17 +82,29 @@ class PyLabLibKinesisPiezoControllerWrapper(_bp):
 
     def get_position(self) -> tuple[float, float, float]:
         """Return (x, y, z) position of the piezo in nanometers."""
-        return tuple(self._ctrl.get_output_voltage(channel=c) * self._NM_PER_VOLT
-                     for c in self._channels)
+        return tuple(
+            self._ctrl.get_output_voltage(channel=c) * self._NM_PER_VOLT
+            for c in self._channels
+        )
 
     def set_position_xy(self, x: float, y: float):
         """Move to xy position specified in nanometers."""
-        [self._ctrl.set_output_voltage(pos / self._NM_PER_VOLT, channel=c) for
-         (pos, c) in zip((x, y,), self._channels[:2])]
+        [
+            self._ctrl.set_output_voltage(pos / self._NM_PER_VOLT, channel=c)
+            for (pos, c) in zip(
+                (
+                    x,
+                    y,
+                ),
+                self._channels[:2],
+            )
+        ]
 
     def set_position_z(self, z: float):
         """Move to z position, specified in nanometers."""
-        self._ctrl.set_output_voltage(z / self._NM_PER_VOLT, channel=self._channels[2])
+        self._ctrl.set_output_voltage(
+            z / self._NM_PER_VOLT, channel=self._channels[2]
+        )
 
     def close(self):
         """Shutdown everything."""
